c239445ed69af313c0a6cc8cefe48b359f78b6cf
Andriy Gnennyy <andriy.gnennyy@globallogic.com>
2018-06-07 23:21:59 +0300

    Npm repository for Nexus and first working build

--- setup-infrastructure.sh
+++ setup-infrastructure.sh
@@ -1,3 +1,22 @@
+function add_npm_proxy() {
+  local _REPO_ID=$1
+  local _NEXUS_USER=$2
+  local _NEXUS_PWD=$3
+  local _NEXUS_URL=$4
+
+  read -r -d '' _REPO_JSON << EOM
+{
+  "name": "$_REPO_ID",
+  "type": "groovy",
+  "content": "repository.createNpmProxy('$_REPO_ID',' https://registry.npmjs.org/')"
+}
+EOM
+
+  curl -v -H "Accept: application/json" -H "Content-Type: application/json" -d "$_REPO_JSON" -u "$_NEXUS_USER:$_NEXUS_PWD" "${_NEXUS_URL}/service/siesta/rest/v1/script/"
+  curl -v -X POST -H "Content-Type: text/plain" -u "$_NEXUS_USER:$_NEXUS_PWD" "${_NEXUS_URL}/service/siesta/rest/v1/script/$_REPO_ID/run"
+}
+
+
 oc new-project gl-oc-demo-ci-cd --display-name="GL OC Demo CI\CD Infra"
 oc new-project gl-oc-demo-ci-cd-dev --display-name="GL OC Demo CI\CD Dev"
 
@@ -18,6 +37,9 @@ oc create -f temp/nexus3.yaml -n gl-oc-demo-ci-cd
 sleep 5
 oc set resources dc/nexus --limits=cpu=1,memory=2Gi --requests=cpu=200m,memory=1Gi -n gl-oc-demo-ci-cd
 oc rollout status dc nexus -n gl-oc-demo-ci-cd
+
+# Nexus Repos
+add_npm_proxy npm admin admin123 "http://nexus-gl-oc-demo-ci-cd.glpractices.com"
 
 # Create Front-End Build(s)
 DEV_FT_TEMPLATE="https://raw.githubusercontent.com/andriy-gnennyy-gl/oc-demo-ci-cd-infrastructure/master/dev-builds-template-front-end.yaml"
